990d4ee03911360b06ad67fb85c1efa3450f46c4
jp-ymatsumoto <y.matsumoto@iwate-designer.com>
2024-08-14 20:23:41 +0900

    Add: ログイン機能とログアウト機能

---
+++ frontend/src/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import { LogoutButton } from "@/components/LogoutButton";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      <h2>プロフィールページ</h2>
+      <LogoutButton />
+    </div>
+  );
+};
+
+export default ProfilePage;
